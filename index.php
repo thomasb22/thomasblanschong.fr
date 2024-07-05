469f30b1a302f95950b657d46bdd0bbcf6322dbe
Thomas Blanschong <contact@thomasblanschong.fr>
2024-07-05 19:02:48 +0200

    Nouvelle version du site

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
 session_start();
 $_SESSION['captcha'] = array( mt_rand(0, 9), mt_rand(1, 9) );
-
-require('config.php');
 ?>
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
 		<meta charset="utf-8">
-		<title>Développeur/Intégrateur Web Freelance à Lannion, Côtes-d'Armor, Bretagne (Wordpress, HTML5, CSS3, JavaScript, jQuery, PHP, Python, MySQL) - Thomas Blanschong</title>
+		<title>Création de site web indépendant à Lannion, Côtes-d'Armor, Bretagne - Thomas Blanschong</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="Thomas Blanschong : Développeur Web Freelance à Lannion, Côtes-d'Armor, Bretagne (Wordpress, HTML5, CSS3, JavaScript, jQuery, PHP, Python, MySQL)">
-		<meta name="keywords" content="developpeur web, freelance, web, internet, intégrateur, création, site internet, developpeur, site web, worpress, php, python, html, css, javascript">
+		<meta name="description" content="Thomas Blanschong : Création de site web indépendant à Lannion, Côtes-d'Armor, Bretagne">
+		<meta name="keywords" content="developpeur web, freelance, web, internet, intégrateur, création, site internet, developpeur, site web, worpress">
 		<meta name="theme-color" content="#ffffff">
 		<meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1">
 		<!-- Meta Og (facebook) -->
@@ -19,30 +17,30 @@ require('config.php');
 		<link rel="image_src" href="https://thomasblanschong.fr/img/carte-site.webp">
 		<meta property="og:locale" content="fr_fr">
 		<meta property="og:site_name" content="Thomas Blanschong - Développeur Web">
-		<meta property="og:title" content="Thomas Blanschong : Développeur Web Freelance à Lannion, Côtes-d'Armor, Bretagne (Wordpress, HTML5, CSS3, JavaScript, jQuery, PHP, Python, MySQL)">
-		<meta property="og:description" content="Thomas Blanschong : Développeur Web Freelance à Lannion, Côtes-d'Armor, Bretagne (Wordpress, HTML5, CSS3, JavaScript, jQuery, PHP, Python, MySQL)">
+		<meta property="og:title" content="Thomas Blanschong : Création de site web indépendant à Lannion, Côtes-d'Armor, Bretagne">
+		<meta property="og:description" content="Thomas Blanschong : Création de site web indépendant à Lannion, Côtes-d'Armor, Bretagne">
 		<meta property="og:type" content="article">
 		<meta property="og:url" content="https://thomasblanschong.fr/">
 		<!-- Card (twitter) -->
 		<meta name="twitter:card" content="summary_large_image">
-		<meta name="twitter:description" content="Thomas Blanschong : Développeur Web Freelance à Lannion, Côtes-d'Armor, Bretagne (Wordpress, HTML5, CSS3, JavaScript, jQuery, PHP, Python, MySQL)">
+		<meta name="twitter:description" content="Thomas Blanschong : Création de site web indépendant à Lannion, Côtes-d'Armor, Bretagne">
 		<meta name="twitter:image" content="https://thomasblanschong.fr/img/carte-site.webp">
-		<meta name="twitter:title" content="Thomas Blanschong : Développeur Web Freelance à Lannion, Côtes-d'Armor, Bretagne (Wordpress, HTML5, CSS3, JavaScript, jQuery, PHP, Python, MySQL)">
+		<meta name="twitter:title" content="Thomas Blanschong : Création de site web indépendant à Lannion, Côtes-d'Armor, Bretagne">
 		<meta name="twitter:url" content="https://thomasblanschong.fr/">
 		<!-- Meta (google+) -->
-		<meta itemprop="name" content="Thomas Blanschong : Développeur Web Freelance à Lannion, Côtes-d'Armor, Bretagne (Wordpress, HTML5, CSS3, JavaScript, jQuery, PHP, Python, MySQL)">
-		<meta itemprop="description" content="Thomas Blanschong : Développeur Web Freelance à Lannion, Côtes-d'Armor, Bretagne (Wordpress, HTML5, CSS3, JavaScript, jQuery, PHP, Python, MySQL)">
+		<meta itemprop="name" content="Thomas Blanschong : Création de site web indépendant à Lannion, Côtes-d'Armor, Bretagne">
+		<meta itemprop="description" content="Thomas Blanschong : Création de site web indépendant à Lannion, Côtes-d'Armor, Bretagne">
 		<meta itemprop="image" content="https://thomasblanschong.fr/img/carte-site.webp">
 		<link rel="manifest" href="/manifest.webmanifest">
 		<link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="any">
 		<link rel="icon" href="/img/favicons/favicon.svg" type="image/svg+xml">
 		<link rel="apple-touch-icon" href="/img/favicons/apple-touch-icon.png">
 		<link rel="stylesheet" href="font-awesome/css/all.css">
-		<link rel="stylesheet" href="css/style.css?1">
+		<link rel="stylesheet" href="css/style.css">
 	</head>
 	<body>
 		<header>
-			<a href="#presentation">
+			<a href="/#">
 				<img src="img/logo.svg" alt="Logo">
 			</a>
 			<nav id="menu">
@@ -54,13 +52,15 @@ require('config.php');
 						<a href="#creations">Créations</a>
 					</li>
 					<li>
+						<a href="https://portfolio.thomasblanschong.fr" target="_blank">Portfolio</a>
+					</li>
+					<li>
 						<a href="#a-propos">À Propos</a>
 					</li>
 					<li>
 						<a href="#contact">Contact</a>
 					</li>
 				</ul>
-				<div id="cursor"></div>
 			</nav>
 			<a id="nav-mobile" href="#menu">
 				<i class="fas fa-bars"></i>
@@ -70,149 +70,152 @@ require('config.php');
 			</a>
 		</header>
 		<main>
-			<section id="presentation">
-				<img class="animed" src="img/logos/wordpress.svg" alt="WordPress">
-				<img class="animed" src="img/logos/html5.svg" alt="HTML5">
-				<img class="animed" src="img/logos/css3.svg" alt="CSS3">
-				<img class="animed" src="img/logos/javascript.svg" alt="JavaScript">
-				<img class="animed" src="img/logos/jquery.svg" alt="JQuery">
-				<img class="animed" src="img/logos/symfony.svg" alt="Symfony">
-				<img class="animed" src="img/logos/php.svg" alt="PHP">
-				<img class="animed" src="img/logos/python.svg" alt="Python">
-				<h1>
-					Bonjour, je suis Thomas. Je suis développeur web <i>full stack</i>,<br>spécialisé en <i>front-end</i>.
-				</h1>
-				<a href="#services" class="next">
-					<i class="fas fa-chevron-down" aria-hidden="true"></i>
-				</a>
-			</section>
-			<section id="services">
+			<header>
 				<div>
-					<ul>
-						<li>
-							<h2>
-								Développement de site web
-							</h2>
-							<p>
-								Création de site web sur mesure. Que cela soit une simple page vitrine, un blog, un site marchant, ou tout autre type de site, je suis en mesure de vous proposer mes services.
-							</p>
-						</li>
-						<li>
-							<h2>
-								Webdesign
-							</h2>
-							<p>
-								Besoin d'un nouveau design pour votre site ? Je peux vous proposer plusieurs maquettes se rapprochant du mieux possible au contenu de votre site, et à votre identité.
-							</p>
-						</li>
-						<li>
-							<h2>
-								Intégration web
-							</h2>
-							<p>
-								Vous avez un nouveau design, mais personne pour l'intégrer à votre site ? Fournissez moi votre maquette, et je l'intégrerai le plus fidèlement possible à votre site.
-							</p>
-						</li>
-						<li>
-							<h2>
-								Développement de modules ou de plugins
-							</h2>
-							<p>
-								Si vous avez besoin d'améliorer votre site, je peux vous proposer le développement ou l'installation de nouveaux modules.
-							</p>
-						</li>
-					</ul>
-					<p>
-						Je peux prendre en charge la totalité de votre projet. De l'étude de celui-ci à son hébergement. Je peux également me charger du dépôt de votre nom de domaine, ainsi que du référencement de votre site sur les moteurs de recherche.
-					</p>
-					<p>
-						Pour tous mes services, je vous accompagnerai et vous conseillerai tout au long du projet.
-					</p>
+					<div>
+						<h1>
+							Création de site web sur mesure
+						</h1>
+						<a class="bouton" href="#services">
+							Mes services
+						</a>
+						<a class="bouton" href="#contact">
+							Me contacter
+						</a>
+					</div>
 				</div>
-				<a href="#creations" class="next">
-					<i class="fas fa-chevron-down" aria-hidden="true"></i>
-				</a>
+			</header>
+			<section id="services">
+				<h2>
+					Mes services
+				</h2>
+				<ul>
+					<li class="devweb">
+						<h3>
+							Développement de site web
+						</h3>
+						<p>
+							Création de site web sur mesure. Que cela soit une simple page vitrine, un blog, un site marchant, ou tout autre type de site, je suis en mesure de vous proposer mes services.
+						</p>
+					</li>
+					<li class="webdesign">
+						<h3>
+							Webdesign
+						</h3>
+						<p>
+							Besoin d'un nouveau design pour votre site ? Je peux vous proposer plusieurs maquettes se rapprochant du mieux possible au contenu de votre site, et à votre identité.
+						</p>
+					</li>
+					<li class="integration">
+						<h3>
+							Intégration web
+						</h3>
+						<p>
+							Vous avez un nouveau design, mais personne pour l'intégrer à votre site ? Fournissez moi votre maquette, et je l'intégrerai le plus fidèlement possible à votre site.
+						</p>
+					</li>
+					<li class="modules">
+						<h3>
+							Développement de modules ou de plugins
+						</h3>
+						<p>
+							Si vous avez besoin d'améliorer votre site, je peux vous proposer le développement ou l'installation de nouveaux modules.
+						</p>
+					</li>
+				</ul>
+				<p>
+					Je peux prendre en charge la totalité de votre projet. De l'étude de celui-ci à son hébergement. Je peux également me charger du dépôt de votre nom de domaine, ainsi que du référencement de votre site sur les moteurs de recherche.
+				</p>
+				<p>
+					Pour tous mes services, je vous accompagnerai et vous conseillerai tout au long du projet.
+				</p>
 			</section>
 			<section id="creations">
+				<h2>
+					Mes créations
+				</h2>
 				<ul>
-<?php
-foreach ($creations as $creation) {
-?>
-					<li id="<?php echo $creation['id']; ?>">
-						<picture>
-							<source srcset="img/creations/<?php echo $creation['shortname']; ?>-500w.webp 500w, img/creations/<?php echo $creation['shortname']; ?>-1920w.webp 1920w" sizes="(max-width: 500px) 500px, 1920px">
-							<img src="img/creations/<?php echo $creation['shortname']; ?>-50w.webp" alt="Capture d'écran du site <?php echo $creation['name']; ?>" loading="lazy">
-						</picture>
-<?php
-	if ($creation['mobile']) {
-?>
-						<img class="smartphone" src="img/smartphone.webp" alt="Smartphone" loading="lazy">
-						<img class="mobile" src="img/creations/<?php echo $creation['shortname']; ?>-mobile.webp" alt="Capture d'écran version mobile du site <?php echo $creation['name']; ?>" loading="lazy">
-<?php
-	}
-?>
-						<aside>
+					<li>
+						<a class="linkpic" href="https://portfolio.thomasblanschong.fr/#bleudechauffe" target="_blank">
 							<div>
-								<h3>
-									<?php echo $creation['name']; ?>
-
-								</h3>
-								<h4>
-									<a href="<?php echo $creation['url']; ?>" target="_blank"><?php echo parse_url($creation['url'], PHP_URL_HOST); ?></a>
-								</h4>
+								<picture>
+									<source srcset="img/creations/bdc-500w.webp 500w, img/creations/bdc-1920w.webp 1920w" sizes="(max-width: 500px) 500px, 1920px">
+									<img src="img/creations/bdc-50w.webp" alt="Capture d'écran du site Bleu de Chauffe" loading="lazy">
+								</picture>
 							</div>
-							<h5>
-								Description :
-							</h5><?php echo $creation['description']; ?>
-
-							<h5>
-								Technologies :
-							</h5>
-							<ul>
-<?php
-	foreach ($creation['technologies'] as $techno) {
-?>
-								<li>
-									<?php echo $techno; ?>
-
-								</li>
-<?php
-	}
-?>
-							</ul>
-						</aside>
+						</a>
+						<div class="mobile">
+							<div>
+								<img src="img/creations/bdc-mobile.webp" alt="Capture d'écran version mobile du site Bleu de Chauffe" loading="lazy">
+							</div>
+						</div>
 					</li>
-<?php
-}
-?>
+					<li>
+						<a class="linkpic" href="https://portfolio.thomasblanschong.fr/#lespaniersdelarosette" target="_blank">
+							<div>
+								<picture>
+									<source srcset="img/creations/lpdlr-500w.webp 500w, img/creations/lpdlr-1920w.webp 1920w" sizes="(max-width: 500px) 500px, 1920px">
+									<img src="img/creations/lpdlr-50w.webp" alt="Capture d'écran du site Les paniers de la Rosette" loading="lazy">
+								</picture>
+							</div>
+						</a>
+						<div class="mobile">
+							<div>
+								<img src="img/creations/lpdlr-mobile.webp" alt="Capture d'écran version mobile du site Les paniers de la Rosette" loading="lazy">
+							</div>
+						</div>
+					</li>
+					<li>
+						<a class="linkpic" href="https://portfolio.thomasblanschong.fr/#studiodumiroir" target="_blank">
+							<div>
+								<picture>
+									<source srcset="img/creations/sdm-500w.webp 500w, img/creations/sdm-1920w.webp 1920w" sizes="(max-width: 500px) 500px, 1920px">
+									<img src="img/creations/sdm-50w.webp" alt="Capture d'écran du site Studio du Miroir" loading="lazy">
+								</picture>
+							</div>
+						</a>
+						<div class="mobile">
+							<div>
+								<img src="img/creations/sdm-mobile.webp" alt="Capture d'écran version mobile du site Studio du Miroir" loading="lazy">
+							</div>
+						</div>
+					</li>
 				</ul>
-				<a href="#" id="creation-previous">
-					<i class="fas fa-chevron-left" aria-hidden="true"></i>
-				</a>
-				<a href="#" id="creation-next">
-					<i class="fas fa-chevron-right" aria-hidden="true"></i>
-				</a>
-				<a href="#a-propos" class="next">
-					<i class="fas fa-chevron-down" aria-hidden="true"></i>
+				<a class="bouton" href="https://portfolio.thomasblanschong.fr/#creations" target="_blank">
+					Plus de créations
 				</a>
 			</section>
+			<blockquote>
+				<h3>
+					Pourquoi parler de <b>site web</b> et non de <b>site Internet</b> ?
+				</h3>
+				<h3>
+					Car le <a href="https://fr.wikipedia.org/wiki/World_Wide_Web" target="_blank" title="Wikipédia" class="help"><b>web</b></a> est un service bien précis, et <a href="https://fr.wikipedia.org/wiki/Internet" target="_blank" title="Wikipédia" class="help"><b>Internet</b></a> est le réseau utilisé par ce service ou un autre.
+				</h3>
+			</blockquote>
 			<section id="a-propos">
+				<h2>
+					À propos
+				</h2>
 				<div>
-					<p>
-						Développeur web depuis 2013, j'ai débuté dans ce domaine pendant mes études en autodidacte en 2005. Améliorant mes acquis grâce à mes études et une veille technologique permanente, je suis à même de vous proposer ces connaissances pour créer ou faire évoluer votre projet.
-					</p>
-					<p>
-						Que cela soit pour un projet entièrement réalisé sur mesure, ou via un <a href="https://fr.wikipedia.org/wiki/Syst%C3%A8me_de_gestion_de_contenu" target="_blank" title="Wikipédia" class="help">CMS</a> soigneusement choisi, je développerai votre projet avec les technologies les plus modernes à ce jour (<a href="https://fr.wikipedia.org/wiki/HTML5" target="_blank" title="Wikipédia" class="help">HTML5</a>, <a href="https://fr.wikipedia.org/wiki/Feuilles_de_style_en_cascade" target="_blank" title="Wikipédia" class="help">CSS3</a>, <a href="https://fr.wikipedia.org/wiki/JavaScript" target="_blank" title="Wikipédia" class="help">JavaScript</a>, <a href="https://fr.wikipedia.org/wiki/Site_web_r%C3%A9actif" target="_blank" title="Wikipédia" class="help">site web réactif</a>).
-					</p>
-					<p>
-						Pour plus d'informations, je vous invite à visionner mon <a href="cv.pdf" target="_blank">CV</a> ou à <a href="#contact">me contacter</a>.
-					</p>
+					<img class="avatar" src="img/avatar.webp" alt="Moi" loading="lazy">
+					<div class="bulle">
+						<p>
+							Bonjour, je suis Thomas Blanschong. Je suis développeur web depuis 2013 et j'ai débuté dans ce domaine pendant mes études en autodidacte en 2005. Améliorant mes acquis grâce à mes études et une veille technologique permanente, je suis à même de vous proposer ces connaissances pour créer ou faire évoluer votre projet.
+						</p>
+						<p>
+							Que cela soit pour un projet entièrement réalisé sur mesure, ou via un <a href="https://fr.wikipedia.org/wiki/Syst%C3%A8me_de_gestion_de_contenu" target="_blank" title="Wikipédia" class="help">CMS</a> soigneusement choisi (par exemple <b>WordPress</b> ou <b>PrestaShop</b>), je développerai votre projet avec les technologies les plus modernes à ce jour (<a href="https://fr.wikipedia.org/wiki/HTML5" target="_blank" title="Wikipédia" class="help">HTML5</a>, <a href="https://fr.wikipedia.org/wiki/Feuilles_de_style_en_cascade" target="_blank" title="Wikipédia" class="help">CSS3</a>, <a href="https://fr.wikipedia.org/wiki/JavaScript" target="_blank" title="Wikipédia" class="help">JavaScript</a>, <a href="https://fr.wikipedia.org/wiki/Site_web_r%C3%A9actif" target="_blank" title="Wikipédia" class="help">site web réactif</a>, etc.).
+						</p>
+						<p>
+							Pour plus d'informations, je vous invite à visionner mon <a href="https://portfolio.thomasblanschong.fr" target="_blank">portfolio</a> ou à <a href="#contact">me contacter</a>.
+						</p>
+					</div>
 				</div>
-				<a href="#contact" class="next">
-					<i class="fas fa-chevron-down" aria-hidden="true"></i>
-				</a>
 			</section>
 			<section id="contact">
+				<h2>
+					Contact
+				</h2>
 				<div>
 					<p>
 						Vous pouvez me suivre via les services suivants : <span id="rs">
@@ -284,12 +287,15 @@ foreach ($creations as $creation) {
 							* Obligatoire
 						</div>
 						<div>
-							<input type="submit" name="submitEmail" id="submitEmail" value="Envoyer">
+							<input class="bouton" type="submit" name="submitEmail" id="submitEmail" value="Envoyer">
 						</div>
 					</form>
 				</div>
 			</section>
 		</main>
+		<footer>
+			<a href="https://gitlab.com/se7h/thomasblanschong.fr" target="_blank">Code source</a>
+		</footer>
 <?php
 if ( isset($_GET['sent']) || isset($_GET['errnom']) || isset($_GET['erremail']) || isset($_GET['errsite']) || isset($_GET['errobjet']) ||
 	isset($_GET['errmess']) || isset($_GET['errcaptcha']) || isset($_GET['errsend']) ) {
