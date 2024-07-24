513c370dbe11f408b858a693a768ab30de761fdf
Thomas Blanschong <contact@thomasblanschong.fr>
2024-07-24 15:33:39 +0200

    Mode sombre

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ $_SESSION['captcha'] = array( mt_rand(0, 9), mt_rand(1, 9) );
 		<link rel="icon" href="/img/favicons/favicon.svg" type="image/svg+xml">
 		<link rel="apple-touch-icon" href="/img/favicons/apple-touch-icon.png">
 		<link rel="stylesheet" href="font-awesome/css/all.css">
-		<link rel="stylesheet" href="css/style.css?v=3">
+		<link rel="stylesheet" href="css/style.css?v=4">
 	</head>
 	<body>
 		<header>
@@ -44,6 +44,15 @@ $_SESSION['captcha'] = array( mt_rand(0, 9), mt_rand(1, 9) );
 				<img src="img/logo.svg" alt="Logo">
 			</a>
 			<nav id="menu">
+				<button class="theme-switcher" type="button" title="Changer le mode de couleurs" aria-pressed="false">
+					<span class="theme-icon">
+						<svg class="theme-icon-light" aria-hidden="true" width="24" height="24" viewBox="0 0 24 24" fill="transparent" stroke="currentColor" stroke-linecap="round">
+							<path fill="currentColor" d="M18 12a6 6 0 1 1-12 0 6 6 0 0 1 12 0Z"/>
+							<path fill="currentColor" fill-rule="evenodd" d="M12 1.25c.41 0 .75.34.75.75v1a.75.75 0 0 1-1.5 0V2c0-.41.34-.75.75-.75ZM4.4 4.4c.3-.3.77-.3 1.06 0l.4.4a.75.75 0 0 1-1.07 1.05l-.4-.4a.75.75 0 0 1 0-1.05Zm15.2 0c.3.3.3.77 0 1.06l-.4.4a.75.75 0 0 1-1.05-1.07l.4-.4a.76.76 0 0 1 1.05 0ZM1.25 12c0-.41.34-.75.75-.75h1a.75.75 0 0 1 0 1.5H2a.75.75 0 0 1-.75-.75Zm19 0c0-.41.34-.75.75-.75h1a.75.75 0 0 1 0 1.5h-1a.75.75 0 0 1-.75-.75Zm-2.1 6.15c.3-.3.77-.3 1.06 0l.4.4a.75.75 0 1 1-1.07 1.05l-.4-.4a.75.75 0 0 1 0-1.05Zm-12.3 0c.3.3.3.77 0 1.06l-.4.4a.75.75 0 1 1-1.05-1.07l.4-.4a.74.74 0 0 1 1.05 0Zm6.15 2.1c.41 0 .75.34.75.75v1a.75.75 0 0 1-1.5 0v-1c0-.41.34-.75.75-.75Z" clip-rule="evenodd"/>
+						</svg>
+						<svg class="theme-icon-dark" aria-hidden="true" width="24" height="24" viewBox="0 0 24 24" fill="transparent" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" /></svg>
+					</span>
+				</button>
 				<ul>
 					<li>
 						<a href="#services">Services</a>
--- js/default.js
+++ js/default.js
@@ -49,3 +49,34 @@ function updateCounter(e) {
 		countermessage.innerHTML = 'caractère restant';
 	counter.innerHTML = maxchar - e.target.value.length;
 }
+
+// Bouton d'activation du mode Sombre / Clair
+// JS Source : https://hidde.blog/dark-light/
+var button = document.querySelector('.theme-switcher');
+var prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+var currentTheme;
+
+if (prefersDark.matches) {
+	currentTheme = 'dark';
+} else {
+	// default
+	currentTheme = 'light';
+}
+
+button.addEventListener('click', function(event) {
+	currentTheme = document.documentElement.getAttribute('data-theme-preference') === "dark" ? "light" : "dark";
+	setTheme(currentTheme);
+});
+
+prefersDark.addEventListener('change', function(event) {
+	currentTheme = event.matches ? 'dark' : 'light';
+	setTheme(currentTheme);
+});
+
+function setTheme(currentTheme) {
+	var pressed = currentTheme === 'dark' ? 'true' : 'false';
+	document.documentElement.setAttribute('data-theme-preference', currentTheme);
+	button.setAttribute('aria-pressed', pressed);
+}
+
+setTheme(currentTheme);
